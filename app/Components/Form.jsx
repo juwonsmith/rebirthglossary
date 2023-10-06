432601b66effb56ac84acecc5b6752928b9f78a1
juwonoladele <juwonoladele772@gmail.com>
2023-10-06 19:39:24 +0100

    work

--- app/Components/Form.jsx
+++ app/Components/Form.jsx
@@ -1,51 +1,45 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useStore } from "../store/useStore";
+
 export default function Form() {
   const setSearchItem = useStore((state) => state.setSearchItem);
-  const inputRef = useRef(null);
+
   const [item, setItem] = useState("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // When scrolling occurs, blur the input field
+      document.activeElement.blur();
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSearchItem(item);
   };
 
-  const handleScroll = () => {
-    if (shouldFocusInput && inputRef.current) {
-      inputRef.current.blur();
-      setShouldFocusInput(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [shouldFocusInput]);
-
   return (
     <div>
-      {" "}
-      <form className="flex justify-center " onSubmit={handleSubmit}>
+      <form className="flex justify-center" onSubmit={handleSubmit}>
         <label className="mt-20 flex flex-col gap-6">
           <input
             type="name"
             className="w-[13rem] ssm:w-[17rem] md:w-[25rem] h-12 text-black rounded-md font-mono p-2"
             placeholder="search keyword here"
             value={item}
-            ref={inputRef}
             onChange={(e) => {
               setItem(e.target.value);
               setSearchItem(e.target.value);
             }}
           />
-          <button
-            className="w-[13rem] ssm:w-[17rem] md:w-[25rem] h-10 font-nunito rounded-md 
-          hover:bg-gradient-to-r hover:from-[#03093f] hover:to-[cyan] bg-[#03093f]  "
-          >
+          <button className="w-[13rem] ssm:w-[17rem] md:w-[25rem] h-10 font-nunito rounded-md hover:bg-gradient-to-r hover:from-[#03093f] hover:to-[cyan] bg-[#03093f]">
             Search
           </button>
         </label>
