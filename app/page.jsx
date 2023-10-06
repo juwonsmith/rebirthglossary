267f9e14eb02d320249adcffff6276d2777dea04
juwonoladele <juwonoladele772@gmail.com>
2023-10-06 18:55:54 +0100

    test

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Form from "./Components/Form";
 import List from "./Components/List";
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-hidden text-white">
+    <main className="min-h-[100dvh] overflow-hidden text-white">
       <Form />
       <List />
     </main>
