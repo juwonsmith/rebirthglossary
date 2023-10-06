c0d7cb7c6dc10907c567a560ead7ed2e819eeca8
juwonoladele <juwonoladele772@gmail.com>
2023-10-06 21:19:47 +0100

    test

--- app/Components/Form.jsx
+++ app/Components/Form.jsx
@@ -1,26 +1,10 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import { useStore } from "../store/useStore";
-
 export default function Form() {
   const setSearchItem = useStore((state) => state.setSearchItem);
 
   const [item, setItem] = useState("");
-  const inputRef = useRef(null);
-  useEffect(() => {
-    const handleScroll = () => {
-      // When scrolling occurs, blur the input field
-      inputRef.current.blur();
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  useEffect(() => {}, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,20 +13,23 @@ export default function Form() {
 
   return (
     <div>
-      <form className="flex justify-center" onSubmit={handleSubmit}>
+      {" "}
+      <form className="flex justify-center " onSubmit={handleSubmit}>
         <label className="mt-20 flex flex-col gap-6">
           <input
             type="name"
             className="w-[13rem] ssm:w-[17rem] md:w-[25rem] h-12 text-black rounded-md font-mono p-2"
             placeholder="search keyword here"
             value={item}
-            ref={inputRef}
             onChange={(e) => {
               setItem(e.target.value);
               setSearchItem(e.target.value);
             }}
           />
-          <button className="w-[13rem] ssm:w-[17rem] md:w-[25rem] h-10 font-nunito rounded-md hover:bg-gradient-to-r hover:from-[#03093f] hover:to-[cyan] bg-[#03093f]">
+          <button
+            className="w-[13rem] ssm:w-[17rem] md:w-[25rem] h-10 font-nunito rounded-md 
+          hover:bg-gradient-to-r hover:from-[#03093f] hover:to-[cyan] bg-[#03093f]  "
+          >
             Search
           </button>
         </label>
