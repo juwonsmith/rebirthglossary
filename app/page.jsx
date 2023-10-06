33b8f566080d7161595d0c4b609ee9a1dcf01cee
juwonoladele <juwonoladele772@gmail.com>
2023-10-06 16:45:37 +0100

    work

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Form from "./Components/Form";
 import List from "./Components/List";
 export default function Home() {
   return (
-    <main className="max-h-full min-h-screen bg-back bg-cover bg-no-repeat overflow-hidden text-white">
+    <main className="max-h-full min-h-[60rem] bg-back bg-cover bg-no-repeat overflow-hidden  text-white">
       <Form />
       <List />
     </main>
