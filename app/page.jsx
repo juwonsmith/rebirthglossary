20516a99801d1ad58d1ade33bcbe4700a9566721
juwonoladele <juwonoladele772@gmail.com>
2023-10-06 18:47:17 +0100

    work

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Form from "./Components/Form";
 import List from "./Components/List";
 export default function Home() {
   return (
-    <main className="max-h-full min-h-screen flex flex-col  overflow-hidden  text-white">
+    <main className="min-h-screen overflow-hidden text-white">
       <Form />
       <List />
     </main>
