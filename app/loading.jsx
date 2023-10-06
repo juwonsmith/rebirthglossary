da05fec7b709410973cb57295104821320544cdb
juwonoladele <juwonoladele772@gmail.com>
2023-10-06 11:29:18 +0100

    mm

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <main className="text-center">
+    <main className="text-center max-h-full min-h-screen bg-back bg-cover bg-no-repeat overflow-hidden text-white">
       <h2 className="">Loading...</h2>
       <p>Hopefully not for to long :)</p>
     </main>
